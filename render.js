d9b7569f8f55464c76b3bdebdf0452bd8907c07d
John Miller <johnpmiller16@gmail.com>
2020-02-19 13:42:47 -0800

    Add results.json

--- render.js
+++ render.js
@@ -1,5 +1,8 @@
 function renderLeaderboard(dataset) {
     // Load the dataset into json
+    $.getJSON("results.json", function(json) {
+        console.log(json); // this will show the info it in firebug console
+    });
 
     // sort by F1 scores for dataset
 
