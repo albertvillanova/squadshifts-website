4f3c524834e6313d01dd90927843316f283afdac
John Miller <johnpmiller16@gmail.com>
2020-02-19 17:49:52 -0800

    Add leaderboards

--- render.js
+++ render.js
@@ -1,17 +1,37 @@
 function renderLeaderboard(dataset) {
-    // Load the dataset into json
-    $.getJSON("results.json", function(json) {
-        console.log(json); // this will show the info it in firebug console
-    });
-
-    // sort by F1 scores for dataset
-
-    // populate scores, human/not, etc
-
-    // need a data.leaderboard field, which is an array of sortered leaderboard
-    // entries
-
+    // Fetch the Mustache template
     var template = document.getElementById('template').innerHTML;
+
+    // Build the leaderboard data
+    var LEADERBOARD_JSON = 'results.json';
+    var rows = [];
+    $.getJSON(LEADERBOARD_JSON, function (data) {
+        // Humans
+        for (var i = 0; i < data.humans.length; i++) {
+            var item = data.humans[i];
+            item.rank = "";
+            item.row_class = "human-row";
+            rows.push(item)
+        }
+        // Models
+        var models = data.models;
+        models.sort(
+            function(a, b){
+                return b[dataset + "_f1"] - a[dataset + "_f1"]
+        });
+        for (var i = 0; i < models.length; i++) {
+            var item = models[i];
+            if(i == 0){
+                item.score_class = "best-score";
+            } else{
+                item.score_class = "";
+            }
+            item.rank = i + 1;
+            item.row_class = ""
+            rows.push(item)
+        }
+    data.leaderboard = rows
     var rendered = Mustache.render(template, data);
     document.getElementById('leaderboard').innerHTML = rendered;
+    });
 }
